d08ec7fa198b6b05a5056af6d757440b969eb7aa
harlo <harlo.holmes@gmail.com>
2014-04-09 10:13:45 -0400

    don't need

---
+++ vars.py
@@ -0,0 +1,12 @@
+from lib.Frontend.vars import *
+
+compass_mime_types = {
+	'pdf' : "application/pdf"
+}
+
+compass_mime_type_map = {
+	'application/pdf' : "pdf"
+}
+
+MIME_TYPES.update(compass_mime_types)
+MIME_TYPES.update(compass_mime_type_map)
