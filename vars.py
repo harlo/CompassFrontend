c5d687b7029bab06e6034ff1b919b6836a9eb23e
harlo <harlo.holmes@gmail.com>
2014-06-09 14:52:14 -0400

    now with task management/reindexing

--- vars.py
+++ vars.py
@@ -8,5 +8,15 @@ compass_mime_type_map = {
 	'application/pdf' : "pdf"
 }
 
+compass_mime_type_tasks = {
+	"application/pdf" : [
+		"PDF.process_metadata.processPDFMetadata", 
+		"PDF.split_pdf_pages.splitPDFPages", 
+		"PDF.extract_pdf_text.extractPDFText", 
+		"Text.preprocess_nlp.preprocessNLP"
+	]
+}
+
 MIME_TYPES.update(compass_mime_types)
-MIME_TYPES.update(compass_mime_type_map)
+MIME_TYPE_MAP.update(compass_mime_type_map)
+MIME_TYPE_TASKS.update(compass_mime_type_tasks)
--- web/js/models/cp_document.js
+++ web/js/models/cp_document.js
@@ -16,18 +16,52 @@ var CompassDocument = UnveillanceDocument.extend({
 			this.set('available_views', []);
 		}
 	},
-	requestReindex: function() {
-	
+	requestReindex: function(el, task_path) {
+		var req = { _id : this.get('_id') }
+		var waiter_span = $($(el).siblings('.cp_waiter')[0]);
+		if(task_path) { _.extend(req, { task_path : task_path })}				
+		
+		$(waiter_span)
+			.html("(indexing...)")
+			.css('display', 'block');
+		
+		doInnerAjax("reindex", "post", req, function(json) {
+			json = JSON.parse(json.responseText);
+			var result = "Could not reindex."
+			if(json.result == 200) {
+				result = "Document reindexed.";
+			}
+			
+			$(waiter_span).html(result);
+			window.setTimeout(function() {
+				$(waiter_span).css('display', 'none');
+			}, 5000);
+		});
 	},
 	setInPanel: function(asset) {
 		var callback = null;
+		switch(asset) {
+			case "reindex":
+				var ctx = this;
+				callback = function() {
+					var tmpl = _.template('<% _.each(tasks, function(task) { %> <li><a onclick="current_document.requestReindex(this, \'<%= task.path %>\');"><%= task.desc %></a><span style="display:none;" class="cp_waiter"></span></li> <% }) %>');
+					var tasks = _.map(UV.MIME_TYPE_TASKS[ctx.get('mime_type')], function(task) {
+						return {
+							path : task,
+							doc_id : ctx.get('_id'),
+							desc: task
+						}
+					});
+					
+					$("#cp_reindex_list").html(tmpl({ tasks : tasks }));
+				};
+				
+				break;	
+		}
 		
 		insertTemplate(
-			asset + ".html", 
-			this.toJSON(), 
-			"#cp_document_view_panel", 
-			callback, 
-			"/web/layout/views/document/");
+			asset + ".html", this.toJSON(), 
+			"#cp_document_view_panel", callback, "/web/layout/views/document/");
 		
 		$.each($("#cp_document_main_ctrl").children('li'), function() {
 			if($(this).attr('id') == "cp_d_" + asset) {
@@ -35,6 +69,6 @@ var CompassDocument = UnveillanceDocument.extend({
 			} else {
 				$(this).removeClass("cp_active");
 			}
-		});
+		});		
 	}
 });
