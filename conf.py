d7a21928a7464b3b5ff088373d137d9366b72dec
harlo <harlo.holmes@gmail.com>
2014-06-12 10:45:51 -0400

    modified file structure

--- conf.py
+++ conf.py
@@ -2,10 +2,12 @@ import os, yaml, json
 
 COMPASS_BASE_DIR = os.path.abspath(os.path.join(__file__, os.pardir))
 COMPASS_CONF_ROOT = os.path.join(COMPASS_BASE_DIR, "conf")
-SECRET_PATH = os.path.join(COMPASS_CONF_ROOT, "compass.secrets.json")
+#SECRET_PATH = os.path.join(COMPASS_CONF_ROOT, "compass.secrets.json")
+
 
 from lib.Frontend.conf import *
 
+SECRET_PATH = os.path.join(CONF_ROOT, "unveillance.secrets.json")
 try:
 	with open(SECRET_PATH, 'rb') as C:
 		config = json.loads(C.read())
--- setup.py
+++ setup.py
@@ -6,22 +6,30 @@ from fabric.operations import prompt
 
 if __name__ == "__main__":
 	base_dir = os.getcwd()
-	cp_step = local("cp conf/compass.secrets.json.example conf/compass.secrets.json")
+	secrets_config = os.path.join(base_dir, "lib", "Frontend", "conf",
+		"unveillance.secrets.json")
+	cp_step = local("cp conf/compass.secrets.json.example %s" % secrets_config)
 	
-	with open("conf/compass.secrets.json", 'rb') as s_conf:
+	with open(secrets_config, 'rb') as s_conf:
+		secrets = loads(s_conf.read())['unveillance.local_remote']
 		try:
-			annex_host = loads(s_conf.read())['unveillance.local_remote']['hostname']
+			annex_host = secrets['hostname']
 		except KeyError as e:
 			print "[ ERROR ] could not find a hostname for remote"
 			exit(1)
+		
+		try:
+			annex_port = secrets['api_port']
+		except KeyError as e:
+			annex_port = 8888
 	 
 	ssh_root = prompt("Type the full path to your .ssh folder and press ENTER\n[DEFAULT ~/.ssh]:")
 	
 	if len(ssh_root) == 0: ssh_root = "~/.ssh"
 
 	os.chdir("lib/Frontend")
-	setup_cmd = "python setup.py '%s' '%s' '%s' 8888 False" % (base_dir, 
-		ssh_root, annex_host)
+	setup_cmd = "python setup.py '%s' '%s' '%s' %d True" % (base_dir, 
+		ssh_root, annex_host, annex_port)
 	setup_step = local(setup_cms)
 	
 	os.chdir("web")
