adcc6fd481c47ade0bf356621024e6d69777abd5
harlo <harlo.holmes@gmail.com>
2014-07-07 14:23:09 -0400

    nitpicking

--- setup.sh
+++ setup.sh
@@ -1,8 +1,6 @@
 #! /bin/bash
 THIS_DIR=`pwd`
 
-pip install --upgrade fabric
-
 cd lib/Frontend
 ./setup.sh $THIS_DIR/conf/compass.secrets.json
 
@@ -13,6 +11,7 @@ cd web
 ln -s $THIS_DIR/web/ extras
 
 cd $THIS_DIR
+chmod 0400 conf/compass.init.json
 chmod 0400 lib/Frontend/conf/unveillance.secrets.json
 chmod 0400 lib/Frontend/conf/local.config.yaml
 python compass_frontend.py -firstuse -webapp
